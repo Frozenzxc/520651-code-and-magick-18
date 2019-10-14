8600a02519b98f517d99f044b557f873c229dfd5
Frozenzxc <39233749+Frozenzxc@users.noreply.github.com>
2019-10-15 00:47:54 +0300

    Добавляет функцию работы с данными с сервера

---
+++ js/backend.js
@@ -0,0 +1,46 @@
+'use strict';
+
+(function () {
+  function load(onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.open('GET', 'https://js.dump.academy/code-and-magick/data');
+    xhr.send();
+  }
+
+  function save(data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.open('POST', 'https://js.dump.academy/code-and-magick');
+    xhr.send(data);
+  }
+
+  window.backend = {
+    load: load,
+    save: save
+  };
+})();
--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,13 @@
 'use strict';
 
 (function () {
+  var WIZADRLIST_COUNT = 4;
   var setup = document.querySelector('.setup');
   var wizardCoat = setup.querySelector('.wizard-coat');
   var wizardEyes = setup.querySelector('.wizard-eyes');
   var fireball = setup.querySelector('.setup-fireball-wrap');
+  var wizardList = document.querySelector('.setup-similar-list');
+  var form = setup.querySelector('.setup-wizard-form');
 
   wizardCoat.addEventListener('click', function () {
     var coatColor = window.util.getRandomElm(window.MOCK.coatColor);
@@ -23,4 +26,37 @@
     fireball.style.background = fireballColor;
     setup.querySelector('input[name = fireball-color]').value = fireballColor;
   });
+
+  function onLoad(wizards) {
+    var data = window.util.getRandomArrElms(wizards, WIZADRLIST_COUNT);
+    data.forEach(function (item) {
+      wizardList.appendChild(window.data.renderWizard(item));
+    });
+
+    setup.querySelector('.setup-similar').classList.remove('hidden');
+  }
+
+  function onError(errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.prepend(node);
+  }
+
+  window.backend.load(onLoad, onError);
+
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), function (response) {
+      if (response) {
+        setup.classList.add('hidden');
+      }
+    }, onError);
+    evt.preventDefault();
+  });
+
 })();
